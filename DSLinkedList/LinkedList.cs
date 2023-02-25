ed150352a7d1c5f7ae0aada544b68b59ba87418b
Siva-Komma <kommasivajyothi22@gmail.com>
2023-02-25 22:13:47 +0530

    Adding Elements into LinkedList

---
+++ DSLinkedList/LinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSLinkedList
+{
+    class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+            Console.WriteLine("{0} is added into LinkedList", newNode.data);
+        }
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+            }
+            else
+            {
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.WriteLine("Linked list is: "+temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
--- DSLinkedList/Program.cs
+++ DSLinkedList/Program.cs
@@ -11,6 +11,11 @@ namespace DSLinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked list in data structure");
+            LinkedList st = new LinkedList();
+            st.Add(56);
+            st.Add(30);
+            st.Add(70);
+            st.Display();
             Console.ReadLine();
         }
     }
